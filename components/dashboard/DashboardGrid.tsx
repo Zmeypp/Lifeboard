cad0edb8fa587471b8b4f315c7f1330293df5894
Zmeypp <mayellelu@gmail.com>
2026-07-16 01:03:03 +0200

    Test MAJ

--- components/dashboard/DashboardGrid.tsx
+++ components/dashboard/DashboardGrid.tsx
@@ -246,7 +246,7 @@ export default function DashboardGrid({
 
       <Card
         delay={0.2}
-        title="Ajouter une opération"
+        title="Ajouter une opération HAHAHA"
         className="col-span-3 row-span-6"
       >
         <OperationForm
